c384c41c0fce328c08bae1f6e1fdce5ecf929d80
Kevin Siwoff <kevin.siwoff@gmail.com>
2015-04-09 12:42:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
+var bgHue = 20, bgSaturation = 100;
+
 function preload(){
 }
 
 function setup() {
   //create our drawing canvas
   createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
 }
 
 function draw() {
-  background(12,14,240);
+  background(bgHue,bgSaturation,240);
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  bgHue = windowWidth % 255;
+  bgSaturation = windowHeight % 255;
 }
